5707b154ef25c56d44fa86707b007ebca6d6c27e
Joy Evans <joyevans@me.com>
2022-04-08 17:11:27 -0500

    created unit test

--- CrownReady_V2/Tests/UnitTest1.cs
+++ CrownReady_V2/Tests/UnitTest1.cs
@@ -1,14 +1,17 @@
 // To create xunit tests, run: dotnet new xunit -n Tests  
 // Once created, run: dotnet add package xUnit  
 using Xunit;
+using Models;
 
 namespace Tests;
-
+// To run test, type `dotnet test`.
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void StoreFrontShouldCreate()
     {
+        Storefront testStoreFront = new Storefront();
 
+        Assert.NotNull(testStoreFront);
     }
 }
