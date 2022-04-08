d47dd122efb23a23551f2e7fca7099e14db3532b
Joy Evans <joyevans@me.com>
2022-04-08 15:42:43 -0500

    modified BL

---
+++ CrownReady_V2/BL/CRBL.cs
@@ -0,0 +1,24 @@
+﻿// first add `using Models`;
+using Models;
+// then add reference `dotnet add reference ../Models`; repeat steps for DL
+using DL;
+namespace BL;
+public class CRBL
+{
+    /// <summary>
+    /// Gets all storefronts
+    /// </summary>
+    /// <returns></returns>
+    public List<Storefront> GetAllStorefronts()
+    {
+        return StaticStorage.GetAllStorefronts();
+    }
+    /// <summary>
+    /// Adds a new storefront to the list
+    /// </summary>
+    /// <param name="storefront">storefront object to add</param>
+    public void AddStorefront(Storefront storefront)
+    {
+        StaticStorage.AddStorefront(storefront);
+    }
+}
