91a6ac5c055c0fbdb9837dcbbfdf73e834dbeb01
Joy Evans <joyevans@me.com>
2022-04-29 10:48:25 -0500

    interface/dependency injection notes

--- CrownReady_V2/UI/Program.cs
+++ CrownReady_V2/UI/Program.cs
@@ -5,4 +5,9 @@ using Models;
 using UI;
 
 MainMenu menu = new MainMenu(new CRBL(new FileRepo()));
+// The below code is the breakdown of the line of code above.
+// IRepo repo = new FileRepo();
+// CRBL bl = new CRBL(repo);
+// MainMenu menu = new MainMenu(bl);
+
 menu.Start();
